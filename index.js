02b4b28959ad84ee138ab8db198170e5dad1ff0d
Zoey <z@zoey.dev>
2023-04-23 11:31:02 -0700

    add random chance of playing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 
   const pipefall = document.getElementById("pipefall");
   const now = document.getElementById("play-now");
+  const playAtRandom = document.getElementById("play-at-random");
+  const randomChance = document.getElementById("random-chance");
+
+  playAtRandom.checked = false;
 
   const play = () => {
     console.log("pipefall!");
@@ -14,4 +18,24 @@
   for (let pipe of document.querySelectorAll(".pipe")) {
     pipe.addEventListener("click", play);
   }
+
+  function random(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  let timeSincePlay = 0;
+  setInterval(() => {
+    if (playAtRandom.checked) {
+      timeSincePlay += 1;
+      if (timeSincePlay >= 60) {
+        const v = random(randomChance.value);
+        if (v === 0) {
+          play();
+          timeSincePlay = 0;
+        }
+      }
+    } else {
+      timeSincePlay = 0;
+    }
+  }, 1000);
 })();
